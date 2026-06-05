1fa3579765d579c58abcd586b40df9ecd549e446
Chris Piekarski <chris@cpiekarski.com>
2026-06-05 16:31:36 -0400

    refactor: migrate sources to Maven standard layout and clean legacy build artifacts
    - Remove old src-java/ tree (moved to src/main/java)
    - Delete deprecated bundled jars (jfree, miglayout, old jna)
    - Update .classpath for new layout
    - Update sub Readme.md references

--- src/hackrf-sweep/src-java/jspectrumanalyzer/Version.java
+++
@@ -1,7 +0,0 @@
-package jspectrumanalyzer;
-
-public class Version
-{
-	public static final String version	= "1.5.0";
-	public static final String url		= "https://github.com/chris-piekarski/hackrf-spectrum-analyzer.git";
-}
